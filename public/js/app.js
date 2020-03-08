49b4f9ea7ae733589edff9a046c76f55d07ee183
Arnav <aal4493@gmail.com>
2020-03-08 16:22:29 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const errorMessage = document.querySelector('#error-message');
+const weatherMessage = document.querySelector('#weather-message');
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    
+    errorMessage.textContent = 'Loading..';
+    weatherMessage.textContent = '';
+
+    const location = search.value;
+    fetch('/weather?address='+location).then((response => {
+        response.json().then((data) => {
+            if(data.error){
+                errorMessage.textContent = data.error;
+            }
+            else{
+                errorMessage.textContent = data.location;
+                weatherMessage.textContent = data.forecast;
+            }
+        });
+    }));
+});
